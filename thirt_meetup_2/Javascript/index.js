caff9c1510ae7bdcf0ae89d5fb572924d9b8cfa8
danka <dana@hlavacova.de>
2025-03-09 10:49:05 +0100

    time work is ready to deploy

--- thirt_meetup_2/Javascript/index.js
+++ thirt_meetup_2/Javascript/index.js
@@ -7,30 +7,32 @@
 //remove this if you don't need it
     let today = new Date(),
         dd = String(today.getDate()).padStart(2, "0"),
-        mm = String(today.getMonth() + 1).padStart(2, "0"),
-        yyyy = today.getFullYear(),
-        nextYear = yyyy + 1,
-        dayMonth = "03/26/",
-        birthday = dayMonth + yyyy;
-    today = mm + "/" + dd + "/" + yyyy;
-    if (today > birthday) {
-        birthday = dayMonth + nextYear;
-    }
-//end
-    const countDown = new Date(birthday).getTime(),
-        x = setInterval(function() {
-            const now = new Date().getTime(),
-                distance = countDown - now;
-            document.getElementById("days").innerText = Math.floor(distance / (day)),
-                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-//do something later when date is reached
-            if (distance < 0) {
-                document.getElementById("countdown").style.display = "none";
-                document.getElementById("content").style.display = "block";
-                clearInterval(x);
-            }
-//seconds
-        }, 0)
+        mm = String(today.getMonth()+1).padStart(2, "0"),
+        yyyy = today.getFullYear()
+
+    const eventDay = 26,   // Den události (9. března)
+        eventMonth = 2,  // Měsíc (0-based, tedy 2 = Březen)
+        eventHour = 18,  // Čas (18:00)
+        eventMinute = 0; // Minuty
+
+    let eventDate = new Date(yyyy, eventMonth, eventDay, eventHour, eventMinute, 0);
+
+    const countDown = eventDate.getTime();
+
+    const x = setInterval(function() {
+        const now = new Date().getTime(),
+            distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / day);
+        document.getElementById("hours").innerText = Math.floor((distance % day) / hour);
+        document.getElementById("minutes").innerText = Math.floor((distance % hour) / minute);
+        document.getElementById("seconds").innerText = Math.floor((distance % minute) / second);
+
+        // Pokud čas vyprší
+        if (distance < 0) {
+            document.getElementById("countdown").style.display = "none";
+            clearInterval(x);
+        }
+
+    }, 1000); // Aktualizace každou sekundu
 }());
